aa485e8d52da9096eaf4652ee05673a1dc1dae8b
Fabricio <fab.assumpcao@gmail.com>
2019-04-10 13:31:38 -0300

    Backend structure

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const express = require ('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+const server = require("http").Server(app);
+const io = require('socket.io')(server);
+
+io.on("connection", socket => {
+    socket.on('connectionRoom', box=> {
+        socket.join(box);
+    })
+} );
+mongoose.connect('mongodb+srv://linkdb:Link123@cluster0-a9shs.mongodb.net/test?retryWrites=true', {
+    useNewUrlParser: true,
+    
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    return next();
+});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true})); //permite o envio de documentos pela URL- será necessário pois estaremos usando para o envio dos documentos
+app.use('/files', express.static(path.resolve(__dirname, '..', 'temp')));
+app.use(require('./routes'));
+
+server.listen(3001);
